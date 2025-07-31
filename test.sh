ac7d00f00d33404b1e016ceef344c76b0d6b2cfe
배명진/인큐베이팅서비스엔지니어링 <mjbae@lguplus.co.kr>
2025-07-31 17:05:12 +0900

    readme 실행 전

--- test.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/zsh
-
-
- curl 'https://typecast.ai/api/actor' \                                                                                                          INT ✘  15:59:30
-  -H 'authorization: Bearer __pltTMazPRLuEVrxNUEQQAiMMt6rhzzMontSSENuQU6Z' | jq
